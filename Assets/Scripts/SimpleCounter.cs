0e2c907e92f95d0f58dc848085cc61ec25be9b2e
Graham Pettinga <gpettinga@gmail.com>
2015-02-27 16:41:31 -0600

    Tried to implement saving of Array.  Failing so far, but I am close.

--- Assets/Scripts/SimpleCounter.cs
+++ Assets/Scripts/SimpleCounter.cs
@@ -31,14 +31,33 @@ public class SimpleCounter : MonoBehaviour
 
 	public void saveTools()
 	{
-		foreach(GameObject Tool in toolObjects)
-		{
-			ToolSpecificInformation toolInforScript = Tool.GetComponent<ToolSpecificInformation>();
 
-			ES2.Save(transform.position, "C:/Users/Field_0001/Desktop/Test_Save_Folder/toolpos"+toolInforScript.toolNumber+".txt");
-			Debug.Log ("Saved Tool Number "+ toolInforScript.toolNumber + Tool.transform.position);
-		}
+		//This Does not work yet, Commenting out for now.GP
+//		ES2.Save(toolObjects.Length,"C:/Users/Field_0001/Desktop/Test_Save_Folder/allTools.txt?tag=toolObjectLength");
+//
+//		for (int i = 0; i < toolObjects.Length; i++)
+//		{
+//			toolObjects[i].SaveIndividualParts(i);
+//		}
+
+
+//		foreach(GameObject Tool in toolObjects)
+//		{
+//			ToolSpecificInformation toolInforScript = Tool.GetComponent<ToolSpecificInformation>();
+//
+//			ES2.Save(transform.position, "C:/Users/Field_0001/Desktop/Test_Save_Folder/toolpos"+toolInforScript.toolNumber+".txt");
+//			Debug.Log ("Saved Tool Number "+ toolInforScript.toolNumber + Tool.transform.position);
+//		}
 	}
+
+	public void SaveIndividualParts(int position)
+	{
+		//ToolSpecificInformation toolInforScript = GetComponent<ToolSpecificInformation>();
+		
+		ES2.Save(transform.position, "C:/Users/Field_0001/Desktop/Test_Save_Folder/toolpos.txt?tag="+position);
+		//Debug.Log ("Saved Tool Number "+ toolInforScript.toolNumber + transform.position);
+	}
+
 	public void LoadTools()
 	{
 		if (ES2.Exists("C:/Users/Field_0001/Desktop/Test_Save_Folder/toolpos"+1+".txt"))

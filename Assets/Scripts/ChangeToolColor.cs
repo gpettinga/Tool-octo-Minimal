25659865148424044ec0e532c9888c0cfc93019c
Graham Pettinga <gpettinga@gmail.com>
2015-09-22 15:56:31 -0500

    Copy tool script and change color fixed

--- Assets/Scripts/ChangeToolColor.cs
+++ Assets/Scripts/ChangeToolColor.cs
@@ -17,30 +17,37 @@ public class ChangeToolColor : MonoBehaviour
 	public void Red()
 	{
 		toolRend.material = toolMats [0];
+		txtColor.color = Color.black;
 	}
 	public void Green()
 	{
 		toolRend.material = toolMats [1];
+		txtColor.color = Color.black;
 	}
 	public void Blue()
 	{
 		toolRend.material = toolMats [2];
+		txtColor.color = Color.black;
 	}
 	public void Yellow()
 	{
 		toolRend.material = toolMats [3];
+		txtColor.color = Color.black;
 	}
 	public void Grey()
 	{
 		toolRend.material = toolMats [4];
+		txtColor.color = Color.black;
 	}
 	public void Purple()
 	{
 		toolRend.material = toolMats [5];
+		txtColor.color = Color.black;
 	}
 	public void White()
 	{
 		toolRend.material = toolMats [6];
+		txtColor.color = Color.black;
 	}
 	public void Black()
 	{
@@ -51,9 +58,21 @@ public class ChangeToolColor : MonoBehaviour
 	public void Orange()
 	{
 		toolRend.material = toolMats [8];
+		txtColor.color = Color.black;
 	}
 	public void LightBlue()
 	{
 		toolRend.material = toolMats [9];
+		txtColor.color = Color.black;
 	}
+
+	public void RedText()
+	{
+		txtColor.color = Color.red;
+	}
+	public void BlackText()
+	{
+		txtColor.color = Color.black;
+	}
+
 }
---
+++ Assets/Scripts/CopyTool.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class CopyTool : MonoBehaviour 
+{
+	private GameObject thisTool;
+	private Vector3 thisPosOffset;
+
+	public void CreateDuplicate()
+	{
+		thisTool = this.gameObject;
+		thisPosOffset = new Vector3 (gameObject.transform.position.x + 1.5f, 0, gameObject.transform.position.z);
+		Instantiate (thisTool, thisPosOffset, Quaternion.identity); 
+
+	}
+
+}

2a5a53fc488bfe070a0a5e00cdb7535d552b3685
Graham Pettinga <gpettinga@gmail.com>
2015-12-04 13:51:56 -0600

    Fixed permanent background stuff.  started working on optimization.

---
+++ Assets/Scripts/DontRenderOffScreen.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DontRenderOffScreen : MonoBehaviour 
+{	
+	private SpriteRenderer sprRend;
+
+	void Start()
+	{
+		sprRend = GetComponent<SpriteRenderer> ();
+	}
+	void OnBecameInvisible()
+	{
+		Debug.Log ("Invisible");
+		sprRend.enabled = false;
+	}
+	void OnBecameVisible()
+	{
+		Debug.Log ("Visible");
+		sprRend.enabled = true;
+	}
+}
